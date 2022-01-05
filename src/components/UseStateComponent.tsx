bbaa4183c834a42d771d660b7d35e0b60e528858
Afrosiyob <45900728+Afrosiyob@users.noreply.github.com>
2022-01-05 18:11:17 +0500

    add
    add

---
+++ src/components/UseStateComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UseStateComponent = () => {
+    return (
+        <div>
+            this is use state component
+        </div>
+    )
+}
+
+export default UseStateComponent
